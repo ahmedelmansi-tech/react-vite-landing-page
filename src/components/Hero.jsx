4d11e104143379602c2f76eda7c682e76b9b1b63
ahmedelmansi-tech <ahmed.helmii911@gmail.com>
2025-11-23 02:27:56 +0200

    hero is done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,10 @@
-import { ChevronDown } from "lucide-react";
+import {
+  ArrowBigRight,
+  ChevronDown,
+  Play,
+  Sparkle,
+  Sparkles,
+} from "lucide-react";
 import { useState, useEffect } from "react";
 import { codeExamples } from "../data/CodeExamples";
 import SyntaxHighlighter from "react-syntax-highlighter";
@@ -35,10 +41,59 @@ const Hero = () => {
 
       {/* --------------------CONTENT--------------------------------------- */}
 
-      <div className="w-full max-w-7xl relative mx-auto">
-        <div className="max-w-7xl mx-auto border flex flex-col lg:grid lg:grid-cols-2 gap-6 sm:gap-8 lg:gap-12 items-center">
-          <div>
-            <h2>HELLO AHMED</h2>
+      <div className="w-full max-w-7xl relative ">
+        <div
+          className="max-w-7xl flex flex-col lg:grid lg:grid-cols-2 gap-6 
+        sm:gap-8 lg:gap-12 items-center"
+        >
+          <div className="text-center lg:text-left">
+            <div className="inline-flex items-center bg-blue-500/10 border-white-500 rounded-full px-4 py-3 space-x-1.5 sm:space-x-2.5 mb-4 sm:mb-6 side-in-from-bottom duration-300">
+              <Sparkles className="w-4 h-4 text-blue-400" />
+              <span className="text-sm sm:text-lg text-blue-300">
+                introduction codeflow ai 2.0
+              </span>
+            </div>
+            <h1 className="text-5xl font-semibold mb-3 sm:mb-4 slide-in-from-bottom aimate-in duration-200 leading-tight">
+              <span
+                className="bg-linear-to-br from-white via-blue-100 to-cyan-100 bg-clip-text text-transparent block mb-3 sm:mb-4
+              "
+              >
+                Code Faster
+              </span>
+              <span
+                className="bg-linear-to-br from-blue-400 via-cyan to-blue-400 bg-clip-text text-transparent block mb-3 sm:mb-4
+              "
+              >
+                Build Better
+              </span>
+              <span
+                className="bg-linear-to-br from-white via-blue-100 to-cyan-100 bg-clip-text text-transparent block mb-3 sm:mb-4
+              "
+              >
+                With CodeFlow AI
+              </span>
+            </h1>
+
+            <p className="text-gray-400 text-md sm:text-base max-w-2xl mb-3 sm:mb-4 slide-in-from-bottom animate-in duration-300 delay-100 leading-relaxed">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+              quod modi mollitia voluptas similique enim non culpa ea debitis
+              molestias.
+            </p>
+            <div className="flex flex-col lg:flex-row items-center lg:justify-start lg:space-x-3">
+              {/* BTN 1 */}
+              <button className="group w-full lg:w-auto bg-linear-to-b from-blue-500 to-blue-300 font-semibold sm:font-base flex items-center  justify-center space-x-1 px-5 py-2 rounded-xl mb-3 trasition-all hover:scale-103 duration-200 delay-200 slide-in-from-bottom ">
+                <span> Start Coding Free </span>
+                <ArrowBigRight className="w-4 h-4 fill-white group-hover:translate-x-1 duration-300" />
+              </button>
+
+              {/* BTN 2 */}
+              <button className="group w-full lg:w-auto bg-white/10 font-semibold sm:font-base flex items-center  justify-center space-x-2.5 px-5 py-2 rounded-xl mb-3 trasition-all hover:bg-white/20 duration-200 delay-300 slide-in-from-bottom">
+                <span className="p-2 border rounded-full group-hover:bg-white/20 group-hover:text-white duration-300 transition-colors">
+                  <Play className="w-4 h-4 fill-white" />
+                </span>
+                <span>Watch Demo</span>
+              </button>
+            </div>
           </div>
           <div className="relative order-2 w-full">
             {/* Outer Layer */}
@@ -67,7 +122,7 @@ const Hero = () => {
                   <div className="flex items-center space-x-1 sm:space-x-2.5 mb-2">
                     <button
                       onClick={() => setTabActive("App.jsx")}
-                      className={`px-4 py-2 ${
+                      className={`cursor-pointer px-4 py-2 ${
                         tabActive === "App.jsx"
                           ? "bg-blue-500/30"
                           : "bg-white/5"
@@ -77,7 +132,7 @@ const Hero = () => {
                     </button>
                     <button
                       onClick={() => setTabActive("Hero.jsx")}
-                      className={`px-4 py-2 ${
+                      className={`cursor-pointer px-4 py-2 ${
                         tabActive === "Hero.jsx"
                           ? "bg-blue-500/30"
                           : "bg-white/5"
@@ -87,7 +142,7 @@ const Hero = () => {
                     </button>
                     <button
                       onClick={() => setTabActive("Navbar.jsx")}
-                      className={`px-4 py-2 ${
+                      className={`cursor-pointer px-4 py-2 ${
                         tabActive === "Navbar.jsx"
                           ? "bg-blue-500/30"
                           : "bg-white/5"
@@ -107,6 +162,7 @@ const Hero = () => {
                         fontSize: "11px",
                         border: "1px solid #3c3c3c",
                         lineHeight: "1.4",
+                        borderRadius: "10px",
                       }}
                     >
                       {codeExamples[tabActive]}
