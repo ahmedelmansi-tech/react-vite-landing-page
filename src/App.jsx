487d165e7e96abfc90136382203d67de8f58663e
ahmedelmansi-tech <ahmed.helmii911@gmail.com>
2025-11-25 17:04:30 +0200

    added the footer and done

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <Features />
       <Pricing />
       <Testimonials />
-      {/*  <Footer /> */}
+      <Footer />
     </h1>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,15 @@
 const Footer = () => {
-  return <div>Footer &copy;</div>;
+  return (
+    <section>
+      <div className="max-w-7xl mx-auto">
+        <div className=" p-9 flex justify-center">
+          <span className="text-2xl text-gray-200 hover:text-gray-300">
+            All rights reserved &copy;
+          </span>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Footer;
